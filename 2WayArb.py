61c908a36ec64d780d478e137cb507fd985db969
craigmiller24 <s2049878@ed.ac.uk>
2025-04-26 20:37:07 +0100

    bug fix

--- 2WayArb.py
+++ 2WayArb.py
@@ -134,10 +134,10 @@ if __name__ == "__main__":
 
     st.header("Plot ROI vs Second Outcome Odds")
     o1 = st.number_input("Input Known Odds (Decimal)", min_value=1.01, value=1.01, step=0.01)
-    o2 = st.slider("Odds for Outcome 2 (Decimal)", min_value=1.01, max_value=500.0, value=1.01, step=0.01)
     max_o2 = st.slider("Max Odds for Outcome 2 (Decimal)", min_value=20.0, max_value=500.0, value=100.0, step=0.01)
+    o2_range = np.linspace(1.01, max_o2, 500)
+    o2 = st.slider("Odds for Outcome 2 (Decimal)", options=o2_range, value=1.01)
 
-    o2_range = np.linspace(1, max_o2, 500)
     roi_values = []
 
     for o2_val in o2_range:
