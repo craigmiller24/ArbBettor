84342004f1370349c15a633cd12ddfbff8d2f12d
craigmiller24 <s2049878@ed.ac.uk>
2025-04-26 20:21:50 +0100

    bug fix

--- 2WayArb.py
+++ 2WayArb.py
@@ -134,7 +134,11 @@ if __name__ == "__main__":
 
     st.header("Plot ROI vs Second Outcome Odds")
     o1 = st.number_input("Input Known Odds (Decimal)", min_value=1.01, value=1.01, step=0.01)
+    o2 = st.number_input("Odds for Outcome 2 (Decimal)", min_value=1.01, value=1.01, step=0.01)
     max_o2 = st.slider("Max Odds for Outcome 2 (Decimal)", min_value=20.0, max_value=500.0, value=100.0, step=0.01)
+    T = st.number_input("Total Stake (£)", min_value=0.01, value=100.0, step=1.0)
+
+
     o2_range = np.linspace(1, max_o2, 500)
     roi_values = []
 
@@ -157,9 +161,6 @@ if __name__ == "__main__":
     ax.grid(True)
 
     st.pyplot(fig)
-
-    o2 = st.number_input("Odds for Outcome 2 (Decimal)", min_value=1.01, value=1.01, step=0.01)
-    T = st.number_input("Total Stake (£)", min_value=0.01, value=100.0, step=1.0)
 
     if st.button("Calculate Optimal Stakes"):
         bets, payout, profit = process_bets(o1, o2, T)
